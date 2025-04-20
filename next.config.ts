95031781aca8c9110118bb7576918b21f89d9c76
Michael Ballen <maikolbb97@gmail.com>
2025-04-21 09:58:09 +1000

    Update dependencies to latest versions, modify image configuration in Next.js, and enhance package.json for improved compatibility and performance.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['media.formula1.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.formula1.com',
+      },
+    ],
   },
 };
 
