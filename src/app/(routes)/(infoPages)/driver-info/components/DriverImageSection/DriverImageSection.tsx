076874f31c529f2f7c70c677964d80650ae57000
Michael Ballen <maikolbb97@gmail.com>
2025-05-14 21:43:14 +1000

    Add GoBackPage and GoTeamPage components for improved navigation
    - Introduced `GoBackPage` and `GoTeamPage` components to enhance user navigation across various pages.
    - Updated `DriverImageSection`, `TeamInfoPage`, `SchedulePage`, `DriverStatisticsPage`, and `TeamStatisticsPage` to include these new components for consistent back navigation and team information access.
    - Refactored the `getTeams` function to support team ID retrieval based on driver information.
    - Enhanced UI layout for better user experience and accessibility.

--- src/app/(routes)/(infoPages)/driver-info/components/DriverImageSection/DriverImageSection.tsx
+++ src/app/(routes)/(infoPages)/driver-info/components/DriverImageSection/DriverImageSection.tsx
@@ -2,10 +2,20 @@ import Image from 'next/image';
 import { DriverImageSectionProps } from './DriverImageSection.types';
 import { unstable_ViewTransition as ViewTransition } from 'react';
 import { AddRemoveFavorites } from '@/components/shared/AddRemoveFavorites';
+import GoBackPage from '@/components/shared/GoBackPage/GoBackPage';
+import GoTeamPage from '@/components/shared/GoTeamPage/GoTeamPage';
+import { getTeams } from '@/lib/getTeams';
 
-export default function DriverImageSection({ driverInfo }: DriverImageSectionProps) {
+export default async function DriverImageSection({ driverInfo }: DriverImageSectionProps) {
+  const teams = await getTeams();
+  const teamId = teams?.find((team) => team.name === driverInfo.team)?.id;
+
   return (
     <div className="w-full md:w-1/2 flex flex-col gap-5">
+      <div className="flex justify-between">
+        <GoBackPage />
+        <GoTeamPage teamId={teamId} />
+      </div>
       <ViewTransition name={`driver-image-${driverInfo.id}`}>
         <Image
           src={driverInfo?.driverImageUrl}
--- src/app/(routes)/schedule/[id]/page.tsx
+++ src/app/(routes)/schedule/[id]/page.tsx
@@ -2,6 +2,7 @@ import { getCircuitInfo } from '@/lib/getCircuits';
 import ScheduleResultsTable from '../components/ScheduleResultsTable/ScheduleResultsTable';
 import ScheduleInfo from '../components/ScheduleInfo/ScheduleInfo';
 import ScheduleDescription from '../components/ScheduleDescription/ScheduleDescription';
+import GoBackPage from '@/components/shared/GoBackPage/GoBackPage';
 export default async function SchedulePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const circuitInfo = await getCircuitInfo(id);
@@ -12,6 +13,7 @@ export default async function SchedulePage({ params }: { params: Promise<{ id: s
 
   return (
     <div className="w-full flex flex-col items-start gap-5 pt-12">
+      <GoBackPage href="/schedule" text="Back to schedule" />
       <ScheduleInfo circuitInfo={circuitInfo} />
       <ScheduleResultsTable circuitInfo={circuitInfo} />
       <ScheduleDescription description={circuitInfo.description} />
